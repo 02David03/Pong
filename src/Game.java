907b6a3696222c7b5a6b4711cea0d8588d88cb73
02David03 <47742784+02David03@users.noreply.github.com>
2020-11-17 18:28:23 -0300

    Black BackGround and Player class created

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
@@ -7,11 +9,16 @@ public class Game extends Canvas implements Runnable {
     private final int height = 160;
     private final int scale = 4;
 
+    public Player player;
+
+    public BufferedImage layer = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+
     public Game(){
-        Screen();
+        this.setPreferredSize(new Dimension(width*scale,height*scale));
+        player = new Player();
+        this.Screen();
     }
     public void Screen(){
-        this.setPreferredSize(new Dimension(width*scale,height*scale));
         JFrame screen = new JFrame("Pong");
         screen.setResizable(false);
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,11 +30,35 @@ public class Game extends Canvas implements Runnable {
 
     public static void main(String[] args){
         Game game = new Game();
+        new Thread(game).start();
     }
+    public void tick(){
 
-
+    }
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics graph = layer.getGraphics();
+        graph.setColor(Color.black);
+        graph = bs.getDrawGraphics();
+        graph.drawImage(layer,0,0,width*scale,height*scale,null);
+        player.render(graph);
+        bs.show();
+    }
     @Override
     public void run() {
+        while (true){
+            tick();
+            render();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Player {
+
+    public void tick(){
+
+    }
+    public void render(Graphics graph){
+        graph.setColor(Color.white);
+        graph.fillRect(15,15,20,90);
+    }
+}
