5573e3fb684d8725a67d7d0955835235d498b6ca
02David03 <47742784+02David03@users.noreply.github.com>
2020-11-18 15:35:11 -0300

    Player moves and Screen collisor

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
-    private final int width = 260;
-    private final int height = 160;
+    public final int width = 260;
+    public final int height = 160;
     private final int scale = 4;
 
     public Player player;
 
-    public BufferedImage layer = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+    public BufferedImage layer = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+
+    public static void main(String[] args){
+        Game game = new Game();
+        new Thread(game).start();
+    }
 
     public Game(){
-        this.setPreferredSize(new Dimension(width*scale,height*scale));
-        player = new Player();
         this.Screen();
+        this.addKeyListener(this);
+        player = new Player(15, height*scale/2 - 90);
     }
+
     public void Screen(){
+        this.setPreferredSize(new Dimension(width*scale,height*scale));
         JFrame screen = new JFrame("Pong");
         screen.setResizable(false);
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,13 +37,12 @@ public class Game extends Canvas implements Runnable {
         screen.setVisible(true);
     }
 
-    public static void main(String[] args){
-        Game game = new Game();
-        new Thread(game).start();
-    }
     public void tick(){
-
+        player.tick();
+        if (player.getY() + player.getHeight() > this.height*scale) player.setY(this.height*scale - player.getHeight());
+        else if (player.getY() < 0) player.setY(0);
     }
+
     public void render(){
         BufferStrategy bs = this.getBufferStrategy();
         if (bs == null) {
@@ -60,5 +68,26 @@ public class Game extends Canvas implements Runnable {
             }
         }
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP -> player.setUp(true);
+            case KeyEvent.VK_DOWN -> player.setDown(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP -> player.setUp(false);
+            case KeyEvent.VK_DOWN -> player.setDown(false);
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,11 +2,61 @@ import java.awt.*;
 
 public class Player {
 
-    public void tick(){
+    private boolean up,down;
+    private int width, height;
+    private final int x;
+    private int y;
+    public Player(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.width = 30;
+        this.height = 100;
+    }
+    public boolean getUp(){
+        return up;
+    }
+    public boolean getDown(){
+        return down;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
 
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void tick(){
+        if (up) y-=5;
+        else if (down) y+=5;
     }
     public void render(Graphics graph){
         graph.setColor(Color.white);
-        graph.fillRect(15,15,20,90);
+        graph.fillRect(x,y,width,height);
     }
 }
